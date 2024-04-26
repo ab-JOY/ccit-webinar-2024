bb33f8e8699a809bd82fc908f06e07a984cec3f4
Allen Joy Bueza <allenjoybueza@gmail.com>
2024-04-26 15:28:06 +0800

    part 2

---
+++ index.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', () => {
+    function $(selector) {
+        const element = document.querySelector(selector)
+        return element
+    }
+
+    
+    const passwordField = $("#password")
+    const generatePassword = document.querySelector("#button-addon2")
+    generatePassword, addEventListener('click', (generatePassword) => {
+        generatePassword.preventDefault()
+        const value = "acbdefghijklmnopqrstuvwxyzABCDEGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
+        const len = 16
+
+        let password = ""
+        for (let i = 0; i < len; i++) {
+            const randomNumber = Math.floor(Math.random() * value.length)
+            password += value.charAt(randomNumber)
+        }
+
+        passwordField.value = password
+    })
+})
+
+
